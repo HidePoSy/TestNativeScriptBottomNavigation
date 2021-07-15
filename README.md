083a0393cbf3a3ecee5f4282cf97e10a9c972cdb
WeiKang <weikang.choong@hexainitiative.com>
2021-07-15 17:25:18 +0800

    [] update some files & readme

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 Testing latest NativeScript v7 bottom navigation
 
 This is to test & debug NativeScript bottom navigation issue on v7.
+Tested on iOS.
+To test, run command: 
+tns run ios
+
+Error message: 
+Error in macroTask: Error: Root should be either UIViewController or UIView
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
-import { enableProdMode } from '@angular/core';
-
 import { AppModule } from './app/app.module'
 
-enableProdMode();
 runNativeScriptAngularApp({
   appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
 });
